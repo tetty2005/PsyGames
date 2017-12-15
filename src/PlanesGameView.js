d656fcc837a79c9f5273b60270be4abed3856e84
Tetiana Lebid <tetty2005@mail.ru>
2017-12-15 21:10:24 +0200

    classes settings

--- src/PlanesGameView.js
+++ src/PlanesGameView.js
@@ -1,137 +1,65 @@
 class PlanesGameView {
     constructor () {
-        this.planes = [];
         this.canvas = document.getElementById('game-canvas');
         this.context = this.canvas.getContext('2d');
+
         this.isInGame = false;
         this.gameDuration = 3000;
         this.actionStartTime;
         this.actionMaxDuration = 8000;
+
         this.correctAnswers = 0;
         this.totalActions = 0;
-        this.x;
-        this.y;
         this.red = '#DB2A56';
-        this.green = '#00A179';
         this.backgroundCanvasColor = '#013E57';
-        this.planeheight = 50;
-        this.planeBase = 20;
-        this.planesNumber = 5;
-        this.directions = ['top', 'right', 'bottom', 'left'];
-        this.planeColors = [this.red, this.green];
-        this.planeCoords = [];
-        this.moveDirection;
-        this.planeDirection;
-        this.planeColor;
     }
 
     render () {
-        for (let i = 0; i < 3; i++) {
-            for (let j = 0; j < 3; j++) {
-                let xc = i*70, 
-                    yc = j*70;
-                this.planeCoords.push([xc, yc]);
-            }
-        }
+        this.planesView = new PlanesView(this.context);
 
-        this.initApp();
+        this.planesView.render();
+        this.initListeners();
         this.startGame();
     }
 
-    drawPlane (dx, dy) {
-        let px = this.x + dx, 
-            py = this.y + dy,
-            px1, py1, px2, py2;
-        this.context.fillStyle = this.planeColor;
-        this.context.beginPath();
-        this.context.moveTo(px, py);
-
-        switch (this.moveDirection) {
-            case 'right':
-                px1 = px - this.planeheight,
-                py1 = py - this.planeBase,
-                px2 = px - this.planeheight,
-                py2 = py + this.planeBase;
-                break;
-            case 'left':
-                px1 = px + this.planeheight,
-                py1 = py - this.planeBase,
-                px2 = px + this.planeheight,
-                py2 = py + this.planeBase;
-                break;
-            case 'bottom':
-                px1 = px + this.planeBase,
-                py1 = py - this.planeheight,
-                px2 = px - this.planeBase,
-                py2 = py - this.planeheight;
-                break;
-            case 'top':
-                px1 = px + this.planeBase,
-                py1 = py + this.planeheight,
-                px2 = px - this.planeBase,
-                py2 = py + this.planeheight;
-                break;
-        }
-
-        this.context.lineTo(px1, py1);
-        this.context.lineTo(px2, py2);
-        this.context.fill();         
-    }
-
-    drawPlanes () {
-        this.context.clearRect(0, 0, 700, 500);
-        for (let i = 0; i < this.planesNumber; i++) {
-            this.drawPlane(this.planes[i][0], this.planes[i][1]);
-        }
-    }
-
-    random (from, to) {
-        return Math.floor(Math.random() * (to - from + 1) + from);
-    }
-
-    startAction () {
-        this.x = 245;
-        this.y = 145;
-        this.planeColor = this.planeColors[this.random(0, 1)];
-        this.planeDirection = this.directions[this.random(0, 3)];
-        this.moveDirection = this.directions[this.random(0, 3)];
-        this.planes = [];
-
-        for (let i = 0; i < this.planesNumber; i++) {
-            this.planes.push(this.planeCoords[this.random(0, 8)]);
-        }
-
-        this.actionStartTime = new Date();
-        this.totalActions++;
+    initListeners () {
+        document.addEventListener('keydown', (e) => {
+            if (this.isInGame) {
+                this.onKeyDown(e);
+            }
+        });
     }
 
     startGame () {
         this.totalActions = 0;
         this.correctAnswers = 0;
         this.isInGame = true;
+
         this.startAction();
         this.gameEngineStep();
         setTimeout(()=> this.stopGame(), this.gameDuration);
     }
 
-    stopGame () {
-        this.isInGame = false;
-        let resultText = 'Your score is: ' + this.correctAnswers + ' from ' + this.totalActions;
-        this.showResults(resultText);
+    startAction () {
+        this.planesSettings = this.planesView.renderPlanesSettings();
+        this.actionStartTime = new Date();
+
+        this.totalActions++;
     }
 
-    showResults (resultText) {
-        $('#resultModal p').html(resultText);
-        $('#resultModal .btn-restart-game').one('click', ()=> this.startGame());
-        $('#resultModal').modal('show');
+    gameEngineStep () {
+        if (this.isInGame) {
+            this.gameLoop();
+            requestAnimationFrame(()=> this.gameEngineStep());
+        }
     }
-        
-    initApp () {
-        document.addEventListener('keydown', (e) => {
-            if (this.isInGame) {
-                this.onKeyDown(e);
-            }
-        });
+
+    gameLoop () {
+        if (this.actionStartTime.getTime() + this.actionMaxDuration < (new Date()).getTime()) {
+            this.startAction();
+        }
+
+        this.planesView.drawPlanes();
     }
 
     onKeyDown (e) {
@@ -151,7 +79,9 @@ class PlanesGameView {
             return;
         }
 
-        let checkDirection = (this.planeColor === this.red)? this.planeDirection: this.moveDirection;
+        let checkDirection = (this.planesSettings.planeColor === this.red)? 
+                            this.planesSettings.planeDirection: 
+                            this.planesSettings.moveDirection;
 
         if (checkDirection === keyDirection) {
             this.correctAnswers++;
@@ -163,33 +93,16 @@ class PlanesGameView {
         this.startAction();
     }
 
-    gameEngineStep () {
-        if (this.isInGame) {
-            this.gameLoop();
-            requestAnimationFrame(()=> this.gameEngineStep());
-        }
+    stopGame () {
+        let resultText = 'Your score is: ' + this.correctAnswers + ' from ' + this.totalActions;
+        this.isInGame = false;
+
+        this.showResults(resultText);
     }
 
-    gameLoop () {
-        if (this.actionStartTime.getTime() + this.actionMaxDuration < (new Date()).getTime()) {
-            this.startAction();
-        }
-
-        this.drawPlanes();
-
-        switch (this.planeDirection) {
-            case 'right':
-                this.x += 1;
-                break;
-            case 'left':
-                this.x -= 1;
-                break;
-            case 'bottom':
-                this.y += 1;
-                break;
-            case 'top':
-                this.y -= 1;
-                break;
-        }
+    showResults (resultText) {
+        $('#resultModal p').html(resultText);
+        $('#resultModal .btn-restart-game').one('click', ()=> this.startGame());
+        $('#resultModal').modal('show');
     }
 }
